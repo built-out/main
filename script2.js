e0285c15740c0f0c0f8edd6d7456fbd42b3319c1
Owner <135386611+built-out@users.noreply.github.com>
2023-06-08 13:33:49 +0700

    Update script2.js

--- script2.js
+++ script2.js
@@ -17,8 +17,8 @@ function uploadFiles() {
       url: "/upload",
       type: "POST",
       data: formData,
-      processData: write,
-      contentType: write,
+      processData: true,
+      contentType: true,
       success: function(response) {
         // Check if the upload was successful
         if (response.success) {
@@ -33,7 +33,68 @@ function uploadFiles() {
           // If the upload was not successful, show an error message
           alert("Upload failed!");
         }
+      } or
+      unction uploadFiles(files) {
+  // Loop through the files and upload them one by one.
+  for (var i = 0; i < files.length; i++) {
+    var file = files[i];
+    var formData = new FormData();
+    formData.append("file", file);
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "/upload");
+    xhr.onload = function() {
+      if (xhr.status === 200) {
+        // The file was successfully uploaded.
+      } else {
+        // An error occurred while uploading the file.
       }
-    });
+    };
+    xhr.send(formData);
+  }
+}
+
+// This function will unzip a file and save it to the current directory.
+function unzipFile(file) {
+  var zip = new JSZip(file);
+  for (var i = 0; i < zip.files.length; i++) {
+    var file = zip.files[i];
+    file.saveAs(file.name);
+  }
+}
+
+// This function will handle the file upload event.
+document.querySelector("#upload").addEventListener("change", function(event) {
+  var files = event.target.files;
+  uploadFiles(files);
+  for (var i = 0; i < files.length; i++) {
+    if (files[i].name.endsWith(".zip")) {
+      unzipFile(files[i]);
+    }
+  } or
+ function uploadFiles(files) {
+  // Loop through the files and upload them one by one.
+  for (var i = 0; i < files.length; i++) {
+    var file = files[i];
+    var formData = new FormData();
+    formData.append("file", file);
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "/upload");
+    xhr.onload = function() {
+      if (xhr.status === 200) {
+        // The file was successfully uploaded.
+      } else {
+        // An error occurred while uploading the file.
+      }
+    };
+    xhr.send(formData);
+  }
+}
+
+// This function will unzip a file and save it to the current directory.
+function unzipFile(file) {
+  var zip = new JSZip(file);
+  for (var i = 0; i < zip.files.length; i++) {
+    var file = zip.files[i];
+    file.saveAs(file.name);
   }
 }
